b8fca52184edd1c437baf71c320f26d24ade9400
srimanta <srimantamohnaty@gmail.com>
2017-05-15 11:16:16 +0800

    added

---
+++ Program.cs
@@ -0,0 +1,167 @@
+﻿
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ContactsPrj
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/gmail-dotnet-quickstart.json
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string ApplicationName = "Gmail API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/gmail-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+
+            //UsersResource.MessagesResource.ListRequest messageRequest = service.Users.Messages.List("ALL");
+
+            // List labels.
+            //IList<Label> labels = request.Execute().Labels;
+            //Console.WriteLine("Labels:");
+            //if (labels != null && labels.Count > 0)
+            //{
+            //    foreach (var labelItem in labels)
+            //    {
+
+            //        // Console.WriteLine("{0}", labelItem.Name);
+            //        if (labelItem.Name=="INBOX")
+            //        {
+            //            Console.WriteLine("{0}", labelItem.Name);
+            //            //bool IsSuccess = GetEmailMessageDetails(service.Users.Messages.List("me"));
+            //        }
+            //    }
+            //}
+            //else
+            //{
+                
+            //    Console.WriteLine("No labels found.");
+            //}
+            
+            var query = service.Users.Messages.List("me");
+
+            var mail = query.Execute();
+
+            foreach (var item in mail.Messages)
+            {
+            string From="";
+            string Subject = "";
+            string ReceivedDate ="";
+            Message massage = service.Users.Messages.Get("me", item.Id).Execute();
+            IList <MessagePartHeader> Hrds = massage.Payload.Headers;
+
+                bool chk = false;
+
+
+                foreach (var hrd in Hrds)
+            {
+                    chk = false;  
+            IList<string> lbls = massage.LabelIds;
+            foreach (var lbl in lbls)
+            {
+                        if (lbl == "INBOX")
+                            chk = true;        
+            }
+
+            if (hrd.Name == "From")
+                From = hrd.Value;
+            if (hrd.Name == "Subject")
+                Subject = hrd.Value;
+            if (hrd.Name == "Date")
+                ReceivedDate = hrd.Value;
+            }
+
+                bool inserted = false;
+                if(chk)
+                inserted = InsertToAZURE(From, Subject, "", ReceivedDate);
+                //hello
+               
+            }
+
+            Console.Read();
+            
+        }
+
+
+        private static bool InsertToAZURE(string From, string Subject, string TextBody, string ReceivedDate)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "contactsql.database.windows.net";
+                builder.UserID = "ContactUser";
+                builder.Password = "Mobile123";
+                builder.InitialCatalog = "Contact";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nInsert data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("INSERT INTO [dbo].[STG_Emails]([EmailFrom],[Emailheader],[EmailDetails],Date_last_interaction,Source_Of_Email)");
+                    sb.Append("VALUES (@EmailFrom,@Emailheader, @EmailDetails,@Date_last_interaction,@Source_Of_Email);");
+                    String sql = sb.ToString();
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@EmailFrom", From);
+                        command.Parameters.AddWithValue("@Emailheader", Subject);
+                        command.Parameters.AddWithValue("@EmailDetails", TextBody);
+                        command.Parameters.AddWithValue("@Date_last_interaction", ReceivedDate);
+                        command.Parameters.AddWithValue("@Source_Of_Email", "Gmail");
+                        int rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine(rowsAffected + " row(s) inserted");
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+            return true;
+        }
+
+
+
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using EAGetMail; //add EAGetMail namespace
+using System.Data.SqlClient;
+
+namespace ContactsPrj
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+
+            string curpath = Directory.GetCurrentDirectory();
+            string mailbox = String.Format("{0}\\inbox", curpath);
+
+            // If the folder is not existed, create it.
+            if (!Directory.Exists(mailbox))
+            {
+                Directory.CreateDirectory(mailbox);
+            }
+
+            // Gmail IMAP4 server is "imap.gmail.com"
+            MailServer oServer = new MailServer("imap.gmail.com",
+                        "singhsinghyadav123", "snehsinghyadav", ServerProtocol.Imap4);
+            MailClient oClient = new MailClient("TryIt");
+
+            // Set SSL connection,
+            oServer.SSLConnection = true;
+
+            // Set 993 IMAP4 port
+            oServer.Port = 993;
+
+            try
+            {
+                oClient.Connect(oServer);
+                MailInfo[] infos = oClient.GetMailInfos();
+                for (int i = 0; i < infos.Length; i++)
+                {
+                    MailInfo info = infos[i];
+                    Console.WriteLine("Index: {0}; Size: {1}; UIDL: {2}",
+                        info.Index, info.Size, info.UIDL);
+
+                    // Download email from GMail IMAP4 server
+                    Mail oMail = oClient.GetMail(info);
+
+                    Console.WriteLine("From: {0}", oMail.From.ToString());
+                    Console.WriteLine("Subject: {0}\r\n", oMail.Subject);
+
+                    // Generate an email file name based on date time.
+                    System.DateTime d = System.DateTime.Now;
+                    System.Globalization.CultureInfo cur = new
+                        System.Globalization.CultureInfo("en-US");
+                    string sdate = d.ToString("yyyyMMddHHmmss", cur);
+                    string fileName = String.Format("{0}\\{1}{2}{3}.eml",
+                        mailbox, sdate, d.Millisecond.ToString("d3"), i);
+
+                    // Save email to local disk
+                   // oMail.SaveAs(fileName, true);
+
+                    bool inserted=false;
+                    inserted = InsertToAZURE(oMail);
+
+
+                    // Mark email as deleted in GMail account.
+                   // oClient.Delete(info);
+                }
+
+                // Quit and purge emails marked as deleted from Gmail IMAP4 server.
+                oClient.Quit();
+            }
+            catch (Exception ep)
+            {
+                Console.WriteLine(ep.Message);
+            }
+        }
+
+        private static bool InsertToAZURE(Mail oMail)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "contactsql.database.windows.net";
+                builder.UserID = "ContactUser";
+                builder.Password = "Mobile123";
+                builder.InitialCatalog = "Contact";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nInsert data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("INSERT INTO [dbo].[STGEmails]([EmailFrom],[Emailheader],[EmailDetails],Date_last_interaction,Source_Of_Email)");
+                    sb.Append("VALUES (@EmailFrom,@Emailheader, @EmailDetails,@Date_last_interaction,@Source_Of_Email);");
+                    String sql = sb.ToString();
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@EmailFrom", oMail.From.ToString());
+                        command.Parameters.AddWithValue("@Emailheader", oMail.Subject.Replace("(Trial Version)",""));
+                        command.Parameters.AddWithValue("@EmailDetails", oMail.ReceivedDate + oMail.TextBody);
+                        command.Parameters.AddWithValue("@Date_last_interaction", oMail.ReceivedDate);
+                        command.Parameters.AddWithValue("@Source_Of_Email", "Gmail");
+                        int rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine(rowsAffected + " row(s) inserted");
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        
+    }
+}
